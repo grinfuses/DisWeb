8bc482dbb45d23eda831084fb91777742e06dad2
Javier Naranjo <grinfuses@gmail.com>
2020-06-15 23:42:24 +0200

    Feat(ml): TensorFlow first steps

--- pfm/ml/api/controllers/backendController.js
+++ pfm/ml/api/controllers/backendController.js
@@ -1,25 +1,26 @@
 exports.entrena = function(req, res) {
     
     const tf = require('@tensorflow/tfjs');
-
-    // Optional Load the binding:
-    // Use '@tensorflow/tfjs-node-gpu' if running with GPU.
-    require('@tensorflow/tfjs-node');
-
-    // Train a simple model:
-    const model = tf.sequential();
-    model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
-    model.add(tf.layers.dense({units: 1, activation: 'linear'}));
-    model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
-
-    const xs = tf.randomNormal([100, 10]);
-    const ys = tf.randomNormal([100, 1]);
-
-    model.fit(xs, ys, {
-    epochs: 100,
-    callbacks: {
-        onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
-    }
-    });
+    var data = getData();
+    const model = tf.sequential(); 
+    
+    // Add a single input layer
+    model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+  model.add(tf.layers.dense({units: 50, activation: 'sigmoid'}));
+    
+    // Add an output layer
+    model.add(tf.layers.dense({units: 1, useBias: true}));
+  
+    console.log(data);
     res.json("Prueba Entrena");
   };
+
+async function getData() {
+    const carsDataReq = await fetch('https://cors-anywhere.herokuapp.com/http://ec2-35-180-234-37.eu-west-3.compute.amazonaws.com/repo_data/btc-eur.json');  
+    const carsData = await carsDataReq.json();  
+    const cleaned = carsData.map(value => ({
+      open: value.Open,
+      close: value.Close,
+    }));  
+    return cleaned;
+  }

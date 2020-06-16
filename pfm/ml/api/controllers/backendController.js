6e213726f38ba3640d142f12c0ad1a552fbcc5b3
Javier Naranjo <grinfuses@gmail.com>
2020-06-16 12:31:16 +0200

    Feat(ml): TensorFlow third steps

--- pfm/ml/api/controllers/backendController.js
+++ pfm/ml/api/controllers/backendController.js
@@ -141,8 +141,8 @@ function almacenaVariables(model, normalizationData) {
     console.log("Pendiente y nplus");
     var pendiente_recta = pend.toInt();
     var nplus_valor = nplus.toInt();
-    console.log(pend.toInt());
-    console.log(nplus.print());
+    console.log(pendiente_recta);
+    console.log(nplus_valor);
     return [pendiente_recta,nplus_valor]
   });
   return [pendiente_recta,nplus_valor];

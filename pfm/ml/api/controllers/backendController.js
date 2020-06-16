46cb97fa28c1cb29d89266130c97f8b0dcb0c285
Javier Naranjo <grinfuses@gmail.com>
2020-06-16 11:11:22 +0200

    Feat(ml): TensorFlow second steps

--- pfm/ml/api/controllers/backendController.js
+++ pfm/ml/api/controllers/backendController.js
@@ -1,3 +1,5 @@
+var request = require('request');
+
 exports.entrena = function(req, res) {
     
     const tf = require('@tensorflow/tfjs');
@@ -6,31 +8,54 @@ exports.entrena = function(req, res) {
     
     // Add a single input layer
     model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
-  model.add(tf.layers.dense({units: 50, activation: 'sigmoid'}));
+    model.add(tf.layers.dense({units: 50, activation: 'sigmoid'}));
     
     // Add an output layer
     model.add(tf.layers.dense({units: 1, useBias: true}));
-  
-    console.log(data);
-    res.json("Prueba Entrena");
+    const tensorData = convertToTensor(data);
+    const {inputs, labels} = tensorData;
+    console.log(inputs);
+    res.json(data);
   };
 
 function getData() {
-    
-    var url_get = "https://cors-anywhere.herokuapp.com/http://ec2-35-180-234-37.eu-west-3.compute.amazonaws.com/repo_data/btc-eur.json";
-    var request = require('request');
-    var options = {
-      'method': 'GET',
-      'url':  url_get,
-      'headers': {
-      }
-    };
-    
-    request(options, function (error, response) { 
-      console.log(response);
-      
-        
-    });  
-  
+  const fs = require('fs');
+  var global_data = fs.readFileSync("cotizaciones.json").toString();
+  return global_data;
+}
 
+function convertToTensor(data) {
+  // Wrapping these calculations in a tidy will dispose any 
+  // intermediate tensors.
+  
+  return tf.tidy(() => {
+    // Step 1. Shuffle the data    
+    tf.util.shuffle(data);
+
+    // Step 2. Convert data to Tensor
+    const inputs = data.map(d => d.open)
+    const labels = data.map(d => d.close);
+
+    const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+    const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+    //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+    const inputMax = inputTensor.max();
+    const inputMin = inputTensor.min();  
+    const labelMax = labelTensor.max();
+    const labelMin = labelTensor.min();
+
+    const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+    const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+    return {
+      inputs: normalizedInputs,
+      labels: normalizedLabels,
+      // Return the min/max bounds so we can use them later.
+      inputMax,
+      inputMin,
+      labelMax,
+      labelMin,
+    }
+  });  
 }

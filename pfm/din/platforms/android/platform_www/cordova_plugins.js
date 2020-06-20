010f898a017de0b637142f3d2e4b198d54bb8388
Javier Naranjo <grinfuses@gmail.com>
2020-06-20 08:54:11 +0200

    Adding Firebase plugin

--- pfm/din/platforms/android/platform_www/cordova_plugins.js
+++ pfm/din/platforms/android/platform_www/cordova_plugins.js
@@ -1,6 +1,16 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "cordova-plugin-firebase-lib.FirebasePlugin",
+      "file": "plugins/cordova-plugin-firebase-lib/www/firebase.js",
+      "pluginId": "cordova-plugin-firebase-lib",
+      "clobbers": [
+        "FirebasePlugin"
+      ]
+    }
+  ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-firebase-lib": "3.3.0"
   };
 });
